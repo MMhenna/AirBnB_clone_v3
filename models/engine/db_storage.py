759aead99eac9572f7d203278e6bd33c97e241be
ameurbesbes <ameurbesbes@outlook.com>
2021-09-21 17:11:51 +0100

    db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -3,13 +3,13 @@
 Contains the class DBStorage
 """
 
-from ...models.amenity import Amenity
-from ...models.base_model import BaseModel, Base
-from ...models.city import City
-from ...models.place import Place
-from ...models.review import Review
-from ...models.state import State
-from ...models.user import User
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from os import getenv
 import sqlalchemy
 from sqlalchemy import create_engine
