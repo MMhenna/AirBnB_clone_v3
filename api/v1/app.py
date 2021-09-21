6b26da3d9dc7badae0a06de87707bbd20a5d2693
MMhenna <mhenna.mehdi@gmail.com>
2021-09-21 16:51:39 +0100

    commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,7 +7,7 @@ Sets up Flask application
 from os import getenv
 from flask import Flask
 from ...models import storage
-from ...api.v1.views import app_views
+from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
