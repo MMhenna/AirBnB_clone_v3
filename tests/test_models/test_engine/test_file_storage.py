b9748636de92477cbd65dac15252e436ad594182
ameurbesbes <ameurbesbes@outlook.com>
2021-09-21 16:15:38 +0100

    unitest for file storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -113,3 +113,8 @@ class TestFileStorage(unittest.TestCase):
         with open("file.json", "r") as f:
             js = f.read()
         self.assertEqual(json.loads(string), json.loads(js))
+        @unittest.skipIf(models.storage_t == 'db', "not testing file storage")
+        def test_get(self):
+            """Test that Returns obj based on class name and its ID"""
+        def test_count(self):
+            """Test that Returns the amount of objects"""
