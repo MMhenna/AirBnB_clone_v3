0cd6046017aeab83f135eb4e469587d0872d50f3
MMhenna <mhenna.mehdi@gmail.com>
2021-09-21 21:30:25 +0100

    commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,7 +5,7 @@ Sets up Flask application
 
 
 from os import getenv
-from flask import Flask, jsonify,make_response
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 
@@ -19,11 +19,13 @@ def teardown(self):
     """Closes storage session"""
     storage.close()
 
+
 @app.errorhandler(404)
 def handle_bad_request(e):
-    return make_response(jsonify({"error": "Not found"}),404)
+    return make_response(jsonify({"error": "Not found"}), 404)
+
 
 if __name__ == '__main__':
     api_host = getenv('HBNB_API_HOST', default='0.0.0.0')
     api_port = getenv('HBNB_API_PORT', default=5000)
-    app.run(host=api_host, port=int(api_port), threaded=True,debug=True)
+    app.run(host=api_host, port=int(api_port), threaded=True, debug=True)
