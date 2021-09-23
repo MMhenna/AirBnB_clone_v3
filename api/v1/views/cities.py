cf0a163786fc9fe34b4f05e75aaa07fc294da33c
ameurbesbes <ameurbesbes@outlook.com>
2021-09-23 01:06:52 +0100

    cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -27,7 +27,7 @@ def get_city(state_id):
     return(jsonify(result))
 
 @app_views.route("/cities/<city_id>", methods=["GET"], strict_slashes=False)
-def get_state(city_id):
+def get_cities(city_id):
     """get selecte state"""
     Afsa = storage.get(City, city_id)
     if Afsa is None:
@@ -38,7 +38,7 @@ def get_state(city_id):
 @app_views.route("/cities/<city_id>",
                  methods=["DELETE"],
                  strict_slashes=False)
-def delete_state(city_id):
+def delete_cities(city_id):
     """delete states"""
     delt = storage.get(City, city_id)
     if delt is None:
@@ -50,33 +50,33 @@ def delete_state(city_id):
 
 
 @app_views.route("/states/<state_id>/cities", methods=["POST"], strict_slashes=False)
-def Creates_states(state_id):
+def Creates_cities(state_id):
     """creates states"""
     sta = storage.get(State, state_id)
     if sta is None:
         abort(404)
-    if not request.get_json:
+    if not request.get_json():
         abort(400, description="Not a JSON")
     if "name" not in request.get_json():
         abort(400, description="Missing name")
-    x = request.get_json
+    x = request.get_json()
     new_st = City(**x)
     new_st.state_id = state_id
     new_st.save()
-    return make_response(jsonify(new_st.to_dict), 200)
+    return make_response(jsonify(new_st.to_dict()), 200)
 
 
 @app_views.route("/cities/<city_id>", methods=["PUT"], strict_slashes=False)
-def updated_states(city_id):
+def updated_cities(city_id):
     """update states"""
     i = storage.get(City, city_id)
     if i is None:
         abort(404)
-    if not request.get_json:
+    if not request.get_json():
         abort(400, description="Not a JSON")
-    x = request.get_json
+    x = request.get_json()
     for k, v in x.items():
         if k not in ["id", "created_at", "updated_at", "state_id"]:
             setattr(i, k, v)
             storage.save()
-    return make_response(jsonify(i.to_dict), 200)
+    return make_response(jsonify(i.to_dict()), 200)
